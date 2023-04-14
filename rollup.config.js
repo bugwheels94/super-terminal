4a7af50c05a459f706952ceb037b0601cf66d362
bugwheels94 <bugwheels94@gmail.com>
2023-04-14 06:55:40 +0530

    fix: import of restify-websocket

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ const babelIncludes = ['./src/**/*'];
 const configs = globby.sync(['./src/**', '!./src/**.json']);
 const bundleNpmWorkspacePackages = ['ws'];
 // Generally only put npm workspace packages here
-const bundlePackages = ['restify-websocket'];
+const bundlePackages = [];
 const neverBundlePackages = ['node-pty', 'ws', 'better-sqlite3', 'tcp-port-used', 'express', 'typeorm', 'redis'];
 const shouldBundleLocalFilesTogether = false;
 const isDevelopment = !!process.env.ROLLUP_WATCH;
