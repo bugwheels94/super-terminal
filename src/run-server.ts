6475d2d810ac0db0d48760d98636767295dccca6
Ankit Gautam <ankit.gautam@aptos.com>
2024-01-29 16:46:07 +0530

    fix: add caching

--- src/run-server.ts
+++ src/run-server.ts
@@ -1,2 +1,18 @@
 import { main } from './index';
-main(0, '');
+if (process.argv.length < 3) main(0, '');
+else {
+	const { spawnSync } = require('child_process');
+	const path = require('path');
+	spawnSync('npm', ['run', 'build:electron'], {
+		cwd: path.join(__dirname, '..'),
+		shell: true,
+		stdio: 'inherit',
+	});
+	const packageJson = require('../package.json');
+	spawnSync('gh', ['release', 'upload', 'v' + packageJson.version, 'out/make/zip/darwin/arm64/*.zip'], {
+		cwd: path.join(__dirname, '..'),
+		shell: true,
+		stdio: 'inherit',
+	});
+	console.log('released version', packageJson.version);
+}
